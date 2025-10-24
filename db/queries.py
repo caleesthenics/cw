9e7ba75fb51671c3292f2653601f88abbe79c768
Albert Lee <koresaremi2001@gmail.com>
2025-10-24 13:02:52 +0600

    cw

---
+++ db/queries.py
@@ -0,0 +1,19 @@
+CREATE_TABLE_TASK ="""
+    CREATE TABLE IF NOT EXISTS tasks(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task TEXT NOT NULL,
+    completed INTEGER DEFAULT 0
+    )
+"""
+
+INSERT_TASK = 'INSERT INTO tasks (task) VALUES (?)'
+
+SELECT_TASK = 'SELECT id,task  FROM tasks'
+
+DELETE_TASK = 'DELETE FROM tasks WHERE id = ?'
+
+UPDATE_TASK = 'UPDATE tasks SET task = ? WHERE id = ?'
+
+UPDATE_COMPLETED = 'UPDATE tasks SET completed = ? WHERE id = ?'
+
+DELETE_ALL_TASK = 'DELETE  FROM tasks'
