59b550d2c93f23039f75264705a9b135dc14368b
Albert Lee <koresaremi2001@gmail.com>
2025-10-26 20:26:16 +0600

    hw

--- main.py
+++ main.py
@@ -3,73 +3,80 @@ from db import main_db
 
 
 def main(page: ft.Page):
-    page.title = 'Список покупок'
-    page.theme_mode = ft.ThemeMode.DARK
+    page.title = 'ToDo list'
+    page.theme_mode = ft.ThemeMode.LIGHT
 
     task_list = ft.Column(spacing=10)
 
-    def create_task_row(task_id, task_text):
+    filter_type = 'all'
+
+    def load_task():
+        task_list.controls.clear()
+        for task_id, task_text, completed in main_db.get_tasks(filter_type):
+            task_list.controls.append(create_task_row(task_id=task_id, task_text=task_text, completed=completed))
+        page.update()
+
+    def create_task_row(task_id, task_text, completed):
         task_field = ft.TextField(value=task_text, read_only=True, expand=True)
 
-
-        def set_completed(_):
-            test = checkbox.value
-            if test == 1 :
-                main_db.update_completed(task_id,completed = 1)
-            elif test == 0:
-                main_db.update_completed(task_id , completed = 0)
-
-        checkbox = ft.Checkbox(on_change=set_completed)
-
-
-
-
+        checkbox = ft.Checkbox(value=bool(completed), on_change=lambda e: toggle_task(task_id, e.control.value))
 
         def enable_edit(_):
             task_field.read_only = False
-            task_field.on_submit = save_edit
             task_field.update()
 
-        def save_edit(_):
+        edit_button = ft.IconButton(icon=ft.Icons.EDIT, tooltip="Редактировать", on_click=enable_edit,
+                                    icon_color=ft.Colors.ORANGE_700)
+
+        def save_task(_):
             main_db.update_task(task_id=task_id, new_task=task_field.value)
             task_field.read_only = True
             task_field.update()
             page.update()
 
-        def del_task(_):
+        save_button = ft.IconButton(icon=ft.Icons.SAVE_ALT_ROUNDED, on_click=save_task)
+
+        def delete_task(_):
             main_db.delete_task(task_id)
             load_task()
 
-        enable_button = ft.IconButton(icon=ft.Icons.EDIT, tooltip='Редактировать', on_click=enable_edit)
-        save_button = ft.IconButton(icon=ft.Icons.SAVE_ALT_ROUNDED, on_click=save_edit)
-        del_button = ft.IconButton(icon=ft.Icons.DELETE, on_click=del_task, icon_color=ft.Colors.RED)
+        delete_button = ft.IconButton(icon=ft.Icons.DELETE, on_click=delete_task, icon_color=ft.Colors.RED)
 
-        return ft.Row([checkbox ,task_field, enable_button, save_button, del_button])
-
-    def load_task():
-        task_list.controls.clear()
-        for task_id, task_text in main_db.get_tasks():
-            task_list.controls.append(create_task_row(task_id=task_id, task_text=task_text))
-        page.update()
+        return ft.Row([checkbox, task_field, edit_button, save_button, delete_button])
 
     def add_task(_):
         if task_input.value:
             task = task_input.value
             task_id = main_db.add_task(task)
-            task_list.controls.append(create_task_row(task_id=task_id, task_text=task))
+            task_list.controls.append(create_task_row(task_id=task_id, task_text=task, completed=None))
+
             task_input.value = ''
             page.update()
 
-    def del_all(_):
-        main_db.del_all_tasks()
+    def toggle_task(task_id, is_completed):
+        main_db.update_task(task_id=task_id, completed=int(is_completed))
         load_task()
 
-    del_all_button = ft.ElevatedButton('Удалить все покупки', on_click=del_all)
-    task_input = ft.TextField(label='Введите название товара', expand=True, on_submit=add_task)
-    add_button = ft.IconButton(icon=ft.Icons.ADD, tooltip='Добавить товар', on_click=add_task)
+    def set_filter(filter_value):
+        nonlocal filter_type
+        filter_type = filter_value
+        load_task()
 
-    page.add(ft.Row([task_input, add_button]), task_list,
-             ft.Row([del_all_button], alignment=ft.MainAxisAlignment.END))
+    def delete_completed(_):
+        main_db.del_completed()
+        load_task()
+
+    filter_buttons = ft.Row([
+        ft.ElevatedButton("Все", on_click=lambda e: set_filter('all')),
+        ft.ElevatedButton('В работе', on_click=lambda e: set_filter('uncompleted')),
+        ft.ElevatedButton("Готово", on_click=lambda e: set_filter('completed')),
+        ft.ElevatedButton('Удалить готовые', on_click=delete_completed)
+    ], alignment=ft.MainAxisAlignment.SPACE_EVENLY)
+
+    task_input = ft.TextField(label='Введите новую задачу', expand=True)
+    add_button = ft.IconButton(icon=ft.Icons.ADD, tooltip='Добавить задачу', on_click=add_task)
+
+    page.add(ft.Row([task_input, add_button]), filter_buttons, task_list)
 
     load_task()
 
@@ -77,5 +84,3 @@ def main(page: ft.Page):
 if __name__ == '__main__':
     main_db.init_db()
     ft.app(target=main)
-
-#ya ne stal menyat nazvaniya, sorry(
